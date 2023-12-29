aac93be4239bd21d000903d4a4e22a44d098c9d5
Yatharth <yatharth0903@gmail.com>
2023-12-29 20:00:39 +0530

    setup configured

---
+++ src/app.js
@@ -0,0 +1,34 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app=express()
+
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true,
+}))
+
+app.use(express.json({limit:"16kb"})) 
+app.use(express.urlencoded({extended:true,limit:"16kb" }))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+//routes import 
+import userRouter from './routes/user.routes.js'
+
+
+
+//routes decralaito
+
+//impt point as routes an dcontroleers are diffrent places store
+
+app.use("/api/v1/users",userRouter)
+ // https//:localhost8000/api/v1/users/register
+
+
+
+
+export {app}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-
+import {app} from './app.js'
 dotenv.config({
     path: './.env'
 })
@@ -10,6 +10,14 @@ dotenv.config({
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
 
 
 
